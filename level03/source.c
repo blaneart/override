0d2b29afddd685bf3767ed85d25ff1d7c7911a14
Artem BLANAR <ablanar@e1r3p11.clusters.42paris.fr>
2023-02-25 00:28:23 +0100

    level03 and 06 better code

--- level03/source.c
+++ level03/source.c
@@ -2,24 +2,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// void decrypt()
-// {
+void decrypt()
+{
+    int i =1971092817; // -0x1d(%ebp)
 
-// }
+    long j = 1734767456; // -0x19(%ebp)
 
-// void test(int i, int answer)
-// {
-//     int param1 = i;
-//     int param2 = answer;
-//     param2 -= param1;
-//     if (0x15 > param2)
-//     {
-//         rand();
-//         decrypt();
-//     }
-//     (param2 << 2) + 0x80489f0;
+    long k = 2070311806; //-0x15(%ebp)
 
-// }
+    long f = 862026877; //-0x11(%ebp)
+
+    int p = 0; // -0xd(%ebp)
+
+
+    int u = -1; // -0x2c(%ebp)
+
+
+    if()
+        system("/bin/sh");
+    else
+        puts("\nInvalid Password");
+}
+
+void test(int i, int answer)
+{
+    int param1 = i;
+    int param2 = answer;
+    param2 -= param1;
+    if (0x15 < param2)
+    {
+        decrypt(rand());
+        return ;
+    }
+
+    (param2 << 2) + 0x80489f0;
+
+}
 
 int main()
 {
@@ -32,6 +50,6 @@ int main()
     printf("Password:");
     scanf("%d", &i);
     printf("%d", i);
-    // test(i, 0x1337d00d);
+    test(i, 0x1337d00d);
     return 0;
 }
--- level06/source.c
+++ level06/source.c
@@ -2,10 +2,23 @@
 
 int auth(char *login, unsigned int serial)
 {
-    int i = 0;
-    i =strcspn(login, "\n");
+    unsigned int i = 0;
+    int length;
+    int ret;
+    i = strcspn(login, "\n");
     serial += i;
-
+    length = strnlen(login, 32);
+    if (length - 5 > 0)
+    {
+        if (ptrace(0,1,0,0) == -1)
+        {
+            puts("***********************************");
+            puts("| !! TAMPERING DETECTED !!  |");
+            puts("***********************************");
+            return 1;
+        }
+        serial += 3;
+    }
 }
 
 
@@ -34,3 +47,12 @@ int main()
     return 0;
 
 }
+
+0x1e240
+
+
+0x005f1e94
+
+0x5f0143
+
+0x005f160a
