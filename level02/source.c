11dc29e617a89b727a9fdcd0321ed3f80fa27189
Arseniy SHISHAEV <ashishae@e1r3p9.clusters.42paris.fr>
2023-02-25 00:10:24 +0100

    Add source for level02

--- level02/source.c
+++ level02/source.c
@@ -1,29 +1,66 @@
-
-
-int main(int argc, char **argv)
+int main(void)
 {
+    char username[48]; // 0x70
+    char password[20]; // 0xa0
+    
+    // The actual length of these buffers is not
+    // precisely known from the source.
+    // Buf might contain up to 112 bytes judging
+    // by the address of the next local variable, password, at 0xa0.
+    // However, we know that it is bzero'd to 48 bytes at the beginning
+    // of the program.
+    char buf[48] // 0x110;
 
-    char buf[2600];
-    char *filename = "/home/users/level03/.pass";
-    char username[12];
-    int fd; 
-    int num_read;
-    char password[]
+
+    char *filename = ;
+    int fd = 0; // 0x8 
+    int num_read = 0; // 0xc
     int size_before_endl;
-    fd = fopen(filename, 'r');
-    if (fd < 0)
-        write(1, "ERROR: failed to open password file\n", 36);
-    num_read = fread(buf, 1, 41, fd);
-    size_before_endl = strcspn(buf, "\n");
-    buf[size_before_endl] = '\0';
+
+    bzero(username, 48);
+    bzero(password, 20);
+    bzero(buf, 48);
+
+    fd = fopen("/home/users/level03/.pass", 'r');
+    if (fd == NULL)
+    {
+        fwrite("ERROR: failed to open password file\n", 1, 36, stderr);
+        exit(1);
+    }
+    
+    num_read = fread(password, 1, 41, fd);
+    size_before_endl = strcspn(password, "\n");
+    password[size_before_endl] = '\0';
     if (num_read != 41)
-        write(1, "ERROR: failed to read password file\n", 36);
+    {
+        fwrite("ERROR: failed to read password file\n", 1, 36, stderr);
+        // sic!
+        fwrite("ERROR: failed to read password file\n", 1, 36, stderr);
+        exit(1);
+    }
     fclose(fd)
     puts("===== [ Secure Access System v1.0 ] =====");
-    fgets(username, 100);
-    fgets(password, 100);
-// To do finish with printf and system call
-    if (strncmp())
-    return 0;
+    puts("/***************************************\\");
+    puts("| You must login to access this system. |");
+    puts("\\***************************************/");
+    
+    printf("--[ Username: ");
+    fgets(username, 100, stdin);
+    username[strcspn(username, "\n")] = '\0';
+    
+    printf("--[ Password: ");
+    fgets(buf, 100, stdin);
+    buf[strcspn(buf, "\n")] = '\0';
+
+    puts("*****************************************");
+
+    if (strncmp(password,buf,41) == 0) {
+        printf("Greetings, %s!\n", username);
+        system("/bin/sh");
+        return 0;
+    }
+    printf(username);
+    puts(" does not have access!");
+    exit(1);
 }
 
